fe568819e6413954634b3b0a2a6e41e09bcc4f50
Harshil Bavaria <harshilbb@gmail.com>
2020-06-23 14:03:07 -0500

    Now API can read data from database

---
+++ DeltaVLiveDiagnostics/DeltaDiagnosticsAPI/src/routes/StorePerformanceData.ts
@@ -0,0 +1,72 @@
+import express from "express";
+let url = 'mongodb://localhost:27017'
+let timeStamp = require("mongoose-timestamp")
+const mongoose = require('mongoose')
+mongoose.connect(url)
+//   //{ useNewUrlParser: true, useUnifiedTopology: true },
+      var Schema = mongoose.Schema;
+      let CsvSchema = new Schema({
+      Node: {
+        type: String,
+        default: "0"
+      },
+      Session: {
+        type: Number,
+        default: 0
+      },
+      Run: {
+        type: String,
+        default: "0"
+      },
+      Run_Group:{
+        type: String,
+        default: "0"
+      },
+      Start:{
+        type: Date,
+        default: Date.now()
+      },
+      End:{
+        type: Date,
+        default: Date.now()
+      },
+      Elapsed_Ms:{
+        type: Number,
+        default: 0
+      }
+});
+const router = express.Router();
+router.post("/todos", async (req, res) => {
+        res.send();
+        let jsonObj = req.body
+        saveData(jsonObj,CsvSchema)
+        //readData()
+
+    async function saveData(jsonObj,CsvSchema){
+      for(let index = 0; index < jsonObj.length; index ++){
+        for(let i = 0; i < jsonObj[index].length; i ++){
+          let run_group = jsonObj[index][i]['Run Group']
+          mongoose.plugin(timeStamp)
+          let PerformanceEntry = mongoose.model(run_group,CsvSchema,run_group)
+          await saveColection(PerformanceEntry, jsonObj, index, i)
+    }
+    }
+  }
+async function saveColection(PerformanceEntry, jsonObj, index, i){
+  let newSave = new PerformanceEntry({
+    Node : jsonObj[index][i].Node,
+    Session: jsonObj[index][i].Session,
+    Run: jsonObj[index][i].Run,
+    Run_Group: jsonObj[index][i]['Run Group'],
+    Start: jsonObj[index][i].Start,
+    End: jsonObj[index][i].End,
+    Elapsed_Ms:jsonObj[index][i]['Elapsed Ms']
+  })
+  newSave.save((err, result) => {
+  if (err){
+  console.log(err)
+  }
+  })   
+}
+  })
+export = router;
