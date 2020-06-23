9765f0d9601cfd3a5b28c559d2ba22af766cbe23
Harshil Bavaria <harshilbb@gmail.com>
2020-06-23 14:04:46 -0500

    Now API can read data from the database

--- DeltaVLiveDiagnostics/DeltaDiagnosticsAPI/src/routes/SendPerformanceData.ts
+++ DeltaVLiveDiagnostics/DeltaDiagnosticsAPI/src/routes/SendPerformanceData.ts
@@ -1,37 +1,38 @@
-// import express from 'express'
-// const router = express.Router()
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost:27017')
-//   //{ useNewUrlParser: true, useUnifiedTopology: true },
-//       var Schema = mongoose.Schema;
-//       let CsvSchema = new Schema({
-//       Node: {
-//         type: String,
-//         default: "0"
-//       },
-//       Session: {
-//         type: Number,
-//         default: 0
-//       },
-//       Run: {
-//         type: String,
-//         default: "0"
-//       },
-//       Run_Group:{
-//         type: String,
-//         default: "0"
-//       },
-//       Start:{
-//         type: Date,
-//         default: Date.now()
-//       },
-//       End:{
-//         type: Date,
-//         default: Date.now()
-//       },
-//       Elapsed_Ms:{
-//         type: Number,
-//         default: 0
-//       }
-// });
-// router.get()
+import express from 'express'
+import router from './auth'
+const mongoose = require('mongoose')
+//const router = express.Router()
+const MongoClient = require('mongodb').MongoClient
+let url = 'mongodb://localhost:27017'
+router.get()
+async function getCollectionNames(){
+    let results = []
+    return new Promise((resolve, reject) => {
+      mongoose.connection.db.listCollections().toArray(function (err, names) {
+      for(let index = 0; index < names.length; index ++){
+        if (err) {
+        reject(err);
+        }
+        let name = names[index].name
+        results.push(name)
+        resolve(results)    
+      }
+      return results
+    });
+  })
+  }
+  async function readData(){
+    let newNames = []
+    newNames.push(await getCollectionNames())
+    console.log(newNames)
+    MongoClient.connect(url, function(err, db) {
+      var db = db.db('admin')
+      for(let index = 0; index < newNames[0].length; index ++){
+      db.collection(newNames[0][index], function(err, collection) {
+        collection.find().toArray(function(err, items){
+          console.log(items)
+        })
+      })
+    }
+    })
+  }
